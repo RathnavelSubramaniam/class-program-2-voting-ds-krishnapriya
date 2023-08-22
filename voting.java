67a14c1e72ab4dab03bcbd10308491a1edfcb066
ds-krishnapriya <142791926+ds-krishnapriya@users.noreply.github.com>
2023-08-22 09:49:15 +0000

    program completed

---
+++ voting.java
@@ -0,0 +1,12 @@
+class voting{
+    public static void main(String[]args){
+        int a=20;
+        if(a>=18)
+        {
+            System.out.println("Eligible to vote");
+        }
+            else{
+            System.out.println("Not eligible to vote");
+        }
+    }
+    }
